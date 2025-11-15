bf0656421091e49399a494a9ebea24a829a69af3
VictorFranklinM <victorfranklin2006@gmail.com>
2025-11-14 23:54:59 -0300

    feat: mapeamento básico da entidade do usuario

--- src/main/java/ifce/edu/br/controle_academico/model/entity/Usuario.java
+++ src/main/java/ifce/edu/br/controle_academico/model/entity/Usuario.java
@@ -1,4 +1,15 @@
 package ifce.edu.br.controle_academico.model.entity;
 
+import ifce.edu.br.controle_academico.model.enums.Role;
+import jakarta.persistence.*;
+
+@Entity
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
